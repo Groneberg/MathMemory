5e8a6d5e057811e764f4d0b2d266da432d522dec
Stefan Groneberg <stefan.groneberg@gmx.de>
2026-01-05 14:13:44 +0100

    add and implement equation.dart

---
+++ lib/src/data/model/equation.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+class Equation {
+  final int id;
+  late int term1;
+  late int term2;
+  late String operator;
+  late int result;
+
+  Equation(this.id) {
+    _generateEquation();
+  }
+
+  void _generateEquation() {
+    final random = Random();
+    final operators = ['+', '-', 'x', '÷'];
+    operator = operators[random.nextInt(operators.length)];
+
+    // division
+    if (operator == '÷') { 
+      int factor = random.nextInt(10) + 1;
+      int quotient = random.nextInt(10);
+      term2 = factor;
+      term1 = factor * quotient;
+      result = quotient;
+    
+    // multiplication
+    } else if (operator == 'x') { 
+      term1 = random.nextInt(11);
+      term2 = random.nextInt(11);
+      result = term1 * term2;
+    
+    // subtraction
+    } else if (operator == '-') { 
+      term1 = random.nextInt(21);
+      term2 = random.nextInt(21);
+      if (term1 < term2) {
+        final temp = term1;
+        term1 = term2;
+        term2 = temp;
+      }
+      result = term1 - term2;
+    
+    // addition
+    } else { 
+      term1 = random.nextInt(21);
+      term2 = random.nextInt(21);
+      result = term1 + term2;
+    }
+  }
+
+  String get equationString => "$term1 $operator $term2";
+}
